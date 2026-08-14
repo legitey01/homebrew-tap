2c99480685dadf75ae5b059594be3f79b224778f
listcoder-release <release@list-coder.com>
2026-08-14 00:37:00 -0300

    listcoder 0.1.12

--- listcoder.rb
+++ listcoder.rb
@@ -4,13 +4,13 @@
 class Listcoder < Formula
   desc "ListCoderCLi - terminal-native AI coding assistant"
   homepage "https://github.com/legitey01/List-Coder-CLI-Realeses"
-  version "0.1.11"
+  version "0.1.12"
   license "MIT"
 
   on_linux do
     on_intel do
       url "https://github.com/legitey01/List-Coder-CLI-Realeses/releases/download/v#{version}/listcoder-x86_64-unknown-linux-gnu"
-      sha256 "5bf8070e2aa154ab361412f2a08734435772891e0f7a53853ff2e4a45a73e205"
+      sha256 "f6780780c20777f235ce99e84152ef84af6345cc3195d66bad9f3672d0ee52e4"
     end
     on_arm do
       url "https://github.com/legitey01/List-Coder-CLI-Realeses/releases/download/v#{version}/listcoder-aarch64-unknown-linux-gnu"
