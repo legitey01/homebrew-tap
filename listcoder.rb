408e296229678f48fe6abfdbd5c9fc082b2256c3
listcoder-release <release@list-coder.com>
2026-08-13 23:52:28 -0300

    Add listcoder formula v0.1.4

---
+++ listcoder.rb
@@ -0,0 +1,28 @@
+# typed: false
+# frozen_string_literal: true
+
+class Listcoder < Formula
+  desc "ListCoderCLi - terminal-native AI coding assistant"
+  homepage "https://github.com/legitey01/List-Coder-CLI-Realeses"
+  version "0.1.4"
+  license "MIT"
+
+  on_linux do
+    on_intel do
+      url "https://github.com/legitey01/List-Coder-CLI-Realeses/releases/download/v#{version}/listcoder-x86_64-unknown-linux-gnu"
+      sha256 "942543de6d9d178a5b796c212c865dfcb8d9d82c0b4d8bee402145900f41852c"
+    end
+    on_arm do
+      url "https://github.com/legitey01/List-Coder-CLI-Realeses/releases/download/v#{version}/listcoder-aarch64-unknown-linux-gnu"
+      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+    end
+  end
+
+  def install
+    bin.install "listcoder"
+  end
+
+  test do
+    system "#{bin}/listcoder", "--version"
+  end
+end
